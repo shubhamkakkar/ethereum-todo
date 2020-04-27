e7f387c2875b3f26f45483d2baa713e5fa09341d
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-04-27 11:48:22 +0530

    made connection to metamask and got account

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import Web3 from "web3"
 function App() {
+
+  const [account, setAccount] = useState("");
+
+  useEffect(
+    () => {
+      loadBlockData();
+    },
+    [],
+  )
+
+
+
+  async function loadBlockData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const network = await web3.eth.net.getNetworkType();
+    const [account] = await web3.eth.getAccounts()
+    setAccount(account)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="continer">
+      eth-todo  : {account}
     </div>
   );
 }
